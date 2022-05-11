47b7102167101423989278de00f78e4566a99f50
Joyful3apster <dimoth3pathiniwasam@gmail.com>
2022-05-11 18:50:26 +0200

    Geschafft!!! Ein Dictionary kann erzeugt werden!

--- Web_Scraper/Scrapping/cms_fix.py
+++ Web_Scraper/Scrapping/cms_fix.py
@@ -8,76 +8,55 @@ soup = BeautifulSoup(source, 'lxml')
 
 article = soup.find('div', id='content')
 
-"""Findet Startpunkt um nextSibling für @find_Erwaegung zu nutzen"""
-
-
-def find_Erwaegung(article):
-    for H in article.find_all('h1'):
-        if H.get_text() == 'Erwägungen':
-            return H
-
-
-def find_number(aritcle):
+def find_number(article):
     number = []
     for H in article.find_all('strong'):
         if H is not None and H.name != 'p':
             if H.get_text().replace('.', '').isnumeric():
-                number.append(H)
+                number.append(H.get_text())
     return number
 
 
-def find_text(start):
-    Absatz = []
-    for sibling in start.find_next_siblings():
-        if sibling.name is None or sibling.name != 'p':
-            break
-        else:
-            Absatz.append(sibling.get_text())
+def find_text(article):
 
-    return Absatz
+    """Finde Erwägungen"""
+    for H in article.find_all('h1'):
+        if H.get_text() == 'Erwägungen':
+            start = H
 
+    """Liste mit allen Absatznummern"""
+    Numbers = find_number(article)
 
-""" Erstellt 2 Listen:
- 1) abs = liste mit allen Textbausteinen (inkl. Absatznummer)
- 2) number = liste mit allen Absatznummer 
-    Grund: Nummer nötig zu jedem Absatz, um in der Datenbank richtig abzuspeichern"""
-
-
-def dict_generator(article):
+    """Dict initialisieren"""
     Erwägungen = collections.defaultdict(list)
-    for H in article.find_all('strong'):
-        if H is not None and H.name != 'p':
-            if H.get_text().replace('.', '').isnumeric():
-                Erwägungen[H.get_text()]
-                for sibling in H.find_next_siblings():
-                    Erwägungen[H.get_text()].append(sibling.get_text())
-                    if sibling.next_element.name != 'strong':
-                        Erwägungen[H.get_text()].append(sibling.next_element.get_text())
-                    else:
-                        break
-    print(Erwägungen)
 
+    """Element'Aus den Erwägungen:' überspringen"""
+    start = start.find_next_sibling().find_next_sibling()
 
-dict_generator(article)
+    """Das erste Element initialisieren"""
+    Erwägungen[Numbers[0]].append(start.get_text()[len(Numbers[0]):])
+    Numbers.pop(0)
 
-"""  for sibling in start.find_next_siblings():
-        if sibling.name is None or sibling.name != 'p':
+    """Den nächsten Startpunkt finden"""
+    start = start.find_next_sibling()
+
+    """Algorithmus"""
+    while True:
+        if len(Numbers) == 1:
+            for H in start.find_next_siblings():
+                if start is not None and start.name != 'p':
+                    break
+                else:
+                    Erwägungen[0].append(H.get_text())
             break
+        if start.get_text()[0:len(Numbers[1])].replace('.', '').isnumeric():
+            Erwägungen[Numbers[0]].append(start.get_text()[len(Numbers[0]):])
+            Numbers.pop(0)
+            start = start.find_next_sibling()
         else:
-            Number = sibling.find('strong')
-            number.append(Number)
-    print(number)
-    counter = 0
-    for next_ in number:
-        Erwägungen[next_]
-        for sibling in next_.find_next_siblings():
-            if sibling.name is None or sibling.name != 'p':
-                break
-            if sibling is number[counter + 1]:
-                counter += 1
-                break
-            else:
-                Erwägungen[Number.get_text()].append(next_.text)
-    print(Erwägungen)"""
+            Erwägungen[Numbers[0]].append(start.get_text()[len(Numbers[0]):])
+            start = start.find_next_sibling()
+    print(Erwägungen)
+    return Erwägungen
 
-find_number(article)
+find_text(article)
