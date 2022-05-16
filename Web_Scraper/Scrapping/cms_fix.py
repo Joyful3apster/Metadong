fef56853206521e11eec1d244a77c702fbf90282
Joyful3apster <dimoth3pathiniwasam@gmail.com>
2022-05-16 12:23:08 +0200

    Kleinere Tweaks

--- Web_Scraper/Scrapping/cms_fix.py
+++ Web_Scraper/Scrapping/cms_fix.py
@@ -3,23 +3,22 @@ import collections
 from bs4 import BeautifulSoup
 import requests
 
-source = requests.get("https://bger.li/138-II-440").text
+source = requests.get("https://bger.li/147-II-209").text
 soup = BeautifulSoup(source, 'lxml')
 
 article = soup.find('div', id='content')
 
-"""Finde Erwägungen"""
-
-
-def find_start(article):
-    for H in article.find_all('h1'):
+"""@find_start: Findet h1 Erwägung im Leitentscheid"""
+def find_start(html):
+    for H in html.find_all('h1'):
         if H.get_text() == 'Erwägungen':
             return H
 
-
-def find_number(article):
+"""Unter Erwägungen gibt es nummerische Absätze
+mit tag <strong>, welche nun in die Liste number gespeichert wird"""
+def find_number(html):
     number = []
-    for H in article.find_all('strong'):
+    for H in html.find_all('strong'):
         if H is not None and H.name != 'p':
             if H.get_text().replace('.', '').isnumeric():
                 number.append(H.get_text())
@@ -27,13 +26,12 @@ def find_number(article):
     return number
 
 
-def find_text(start, article):
-
+def find_text(start, html):
     """Liste mit allen Absatznummern"""
-    Numbers = find_number(article)
+    Numbers = find_number(html)
 
     """Dict initialisieren"""
-    Erwägungen = collections.defaultdict(list)
+    Erwaegungen = collections.defaultdict(list)
 
     """Element'Aus den Erwägungen:' überspringen"""
     start = start.find_next_sibling().find_next_sibling()
@@ -43,11 +41,14 @@ def find_text(start, article):
 
         """Handling wenn Numbers genau ein Element noch hat"""
         if len(Numbers) == 1:
-            for H in start.find_next_siblings():
-                if start is not None and start.name != 'p':
-                    break
-                else:
-                    Erwägungen[0].append(H.get_text())
+            Erwaegungen[0].append(start.get_text())
+            start = start.find_next_sibling()
+            if start:
+                for H in start.find_next_siblings():
+                    if start is not None and start.name != 'p':
+                        break
+                    else:
+                        Erwaegungen[0].append(H.get_text())
             break
 
         """Vorkalkulationen"""
@@ -55,24 +56,24 @@ def find_text(start, article):
         num = Numbers[0]
         nextnum = Numbers[1]
         Test_next_line_numeric = temp.get_text()[0:len(nextnum)].replace('.', '').isnumeric()
-        Test_next_line_same = temp.get_text()[0:len(nextnum)] is not start.get_text()[0:len(num)]
+        Test_next_prev_same = temp.get_text()[0:len(nextnum)] is not start.get_text()[0:len(num)]
         Test_line_to_cut = start.get_text()[0:len(num)].replace('.', '').isnumeric()
         to_save_cut = start.get_text()[len(num):]
         to_save = start.get_text()
         """"""""""""""""""""""""""""""
 
-        if Test_next_line_numeric and Test_next_line_same:
+        if Test_next_line_numeric and Test_next_prev_same:
             if Test_line_to_cut:
-                Erwägungen[num].append(to_save_cut)
+                Erwaegungen[num].append(to_save_cut)
             else:
-                Erwägungen[num].append(to_save)
+                Erwaegungen[num].append(to_save)
             Numbers.pop(0)
             start = start.find_next_sibling()
         else:
-            Erwägungen[num].append(to_save)
+            Erwaegungen[num].append(to_save)
             start = start.find_next_sibling()
-    print(Erwägungen)
-    return Erwägungen
+    print(Erwaegungen)
+    return Erwaegungen
 
 
 find_text(find_start(article), article)
